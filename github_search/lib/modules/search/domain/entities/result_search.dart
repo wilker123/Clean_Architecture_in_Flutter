71ecac8fdcfd9d4bf51d155e354080e63fd7037b
wilker123 <josewilkerlucena123@gmail.com>
2021-12-16 13:59:53 -0300

    Created entity ResultSearch

--- github_search/lib/modules/search/domain/entities/result_search.dart
+++ github_search/lib/modules/search/domain/entities/result_search.dart
@@ -1,1 +1,8 @@
-   
+class ResultSearch{
+  final String title;
+  final String content;
+  final String img;
+
+  ResultSearch({required this.title, required this.content, required this.img});
+
+}
