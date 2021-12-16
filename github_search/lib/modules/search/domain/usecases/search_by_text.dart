46d9f13c6bb16dd4a1a864ec5bdb3e7a5844de44
wilker123 <josewilkerlucena123@gmail.com>
2021-12-16 15:50:17 -0300

    Creating search_repository repository contract

--- github_search/lib/modules/search/domain/usecases/search_by_text.dart
+++ github_search/lib/modules/search/domain/usecases/search_by_text.dart
@@ -1,12 +1,21 @@
 import 'package:github_search/modules/search/domain/entities/result_search.dart';
 import 'package:dartz/dartz.dart';
 import 'package:github_search/modules/search/domain/errors/errors.dart';
+import 'package:github_search/modules/search/domain/repositories/search_repository.dart';
 
 abstract class SearchByText{
   Future<Either<FailureSearch,List<ResultSearch>>> call(String searchText);
 }
 
 class SearchByTextImpl implements SearchByText{
+
+  final SearchRepository repository;
+  SearchByTextImpl(this.repository);
+
   @override
- Future<Either<FailureSearch,List<ResultSearch>>> call(String searchText) async{}
+ Future<Either<FailureSearch,List<ResultSearch>>> call(String searchText) async{
+
+
+
+ }
 }
